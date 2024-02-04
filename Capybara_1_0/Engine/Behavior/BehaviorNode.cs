642465efed686688cd05af269991c50d58f92934
Capycanth <dillonlehrer1113@gmail.com>
2024-02-04 16:51:13 -0500

    Behavior

---
+++ Capybara_1_0/Engine/Behavior/BehaviorNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capybara_1.Engine.Behavior
+{
+    public enum ConditionReturnEnum
+    {
+        False = 0,
+        True = 1,
+    }
+
+    public interface IBehaviorNode
+    {
+        Action Execute();
+    }
+
+    public class ConditionNode : IBehaviorNode
+    {
+        private Func<ConditionReturnEnum> _condition;
+        private Dictionary<ConditionReturnEnum, IBehaviorNode> children;
+
+        public ConditionNode(Func<ConditionReturnEnum> condition, Dictionary<ConditionReturnEnum, IBehaviorNode> children)
+        {
+            _condition = condition;
+            this.children = children;
+        }
+
+        public Action Execute()
+        {
+            return children[_condition()].Execute();
+        }
+    }
+
+    public class ActionNode : IBehaviorNode
+    {
+        private Action action;
+
+        public ActionNode(Action action)
+        {
+            this.action = action;
+        }
+
+        public Action Execute()
+        {
+            return action;
+        }
+    }
+}
---
+++ Capybara_1_0/Engine/Behavior/BehaviorTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capybara_1.Engine.Behavior
+{
+    public class BehaviorTree
+    {
+        private IBehaviorNode root;
+
+        public BehaviorTree(IBehaviorNode root)
+        {
+            this.root = root;
+        }
+
+        public void Execute()
+        {
+            root.Execute()();
+        }
+    }
+}
